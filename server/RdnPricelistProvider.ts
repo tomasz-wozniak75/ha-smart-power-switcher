ee761674d340c05a4390b9a69b06057fc5732b88
Tomasz Wozniak <tomasz.wozniak@bp.com>
2024-09-18 17:35:31 +0200

    RdnPricelistProvider 1

--- server/RdnPricelistProvider.ts
+++ server/RdnPricelistProvider.ts
@@ -1,10 +1,20 @@
+import { DateTimeUtils } from "./DateTimeUtils";
 
 
 class RdnPricelistProvider {
-    private priceListCache: { [id: number]: PricelistItem; }
-     
-    getPriceList(forDay: number): Promise<PricelistItem[]> {
-        return new Promise();
+    private priceListCache: { [id: number]: PricelistItem[]; }
 
+    public getPriceList(forDay: number): Promise<PricelistItem[]> {
+        const requestedDate = DateTimeUtils.cutOffTime(forDay);
+
+        if (this.priceListCache[requestedDate]) {
+            return Promise.resolve(this.priceListCache[requestedDate]);
+        }
+        return this.fetchPriceList(requestedDate);
+
+    }
+    
+    private fetchPriceList(requestedDate: number): Promise<PricelistItem[]> {
+        throw new Error("Method not implemented.");
     }
 }
