d8f1b66b5fee46ff86655ccb6cf677600f94d5f9
Tomasz Wozniak <tomasz.wozniak@bp.com>
2024-09-18 17:25:24 +0200

    DateTimeUtils.cutOffTime should cut off time, part 2

--- server/DateTimeUtils.test.ts
+++ server/DateTimeUtils.test.ts
@@ -2,7 +2,11 @@ import { DateTimeUtils } from "./DateTimeUtils";
 
 
 
-test("DateTimeUtils.cutOffTime should cut off time part", () => {
+test("DateTimeUtils.cutOffTime should cut off time, part 1", () => {
     expect(DateTimeUtils.cutOffTime(new Date (2024, 9, 18, 17, 5).getTime())).toBe(new Date (2024,9,18).getTime());
+});
+
+test("DateTimeUtils.cutOffTime should cut off time, part 2", () => {
+    expect(DateTimeUtils.cutOffTime(new Date (2024, 9, 18, 17, 5).getTime())).not.toBe(new Date (2024, 9, 18, 17, 5).getTime());
 });
 
