fb210a6d7e792cb4f48215813e7edfa7c523d15a
Tomasz Wozniak <tomasz.wozniak@bp.com>
2024-09-30 23:01:25 +0200

    Hide past items

--- web-app/src/PricelistTable.tsx
+++ web-app/src/PricelistTable.tsx
@@ -7,10 +7,12 @@ import { ErrorComponent } from './ErrorComponent';
 
 
 export const PriceListTable = () => {
-  const [currentDate] = useState<number>(DateTimeUtils.cutOffTime(Date.now()));
+  const [today] = useState<number>(DateTimeUtils.cutOffTime(Date.now()));
   const [date, setDate] = useState<number>(DateTimeUtils.cutOffTime(Date.now()));
   const [pricelist, setPricelist] = useState<PricelistItem[]>([]);
+  const [filteredPricelist, setFilteredPricelist] = useState<PricelistItem[]>([]);
   const [fetchingError, setFetchingError] = useState<string[]>(null);
+  const [hidePastItems, setHidePastItems] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPriceList = async () => {
@@ -26,30 +28,50 @@ export const PriceListTable = () => {
   fetchPriceList()
       .then((response) => {setFetchingError(null); setPricelist(response); } )
       .catch((error) => {setFetchingError(error.message);});
-
+    setHidePastItems(today === date);
   }, [date]);
+
+  const filterPricelistItem = (pricelistItem: PricelistItem) => today === date && hidePastItems ? (pricelistItem.startsAt + pricelistItem.duration) > Date.now() : true
+
+  const renderHidePastItems = () => {
+    if (today === date) {
+    return ( 
+      <tr>
+        <th colSpan={2}>
+          <label htmlFor="hidePastItems">Hide past items: </label>
+          <input id="hidePastItems" type="checkbox" checked={hidePastItems} onChange={ (event) => setHidePastItems(event.target.checked) }/>
+        </th>
+      </tr>
+    )
+    }
+    return null;
+  }
+
+   useEffect(() => {
+    setFilteredPricelist(pricelist.filter(filterPricelistItem));
+   }, [pricelist, hidePastItems, date]);
 
   return (
     <div>
         <div>
           <label htmlFor="date">Select price list date: </label>
-          <input id="date" type='date' value={DateTimeUtils.formatDateForInput(date)} onChange={ (e) => setDate(new Date(e.target.value).getTime())}/>
+          <input id="date" type='date' value={DateTimeUtils.formatDateForInput(date)} onChange={ (e) => setDate(DateTimeUtils.cutOffTime(new Date(e.target.value).getTime()))}/>
         </div>
         {fetchingError != null ? <ErrorComponent message={fetchingError}/>:
           (
             <table className="pricelistTable">
               <thead>
                 <tr>
-                  <th>Price list for {date === currentDate ? "today" : (date < currentDate ? "the past" : "the future")}: {DateTimeUtils.formatDate(date)}</th>
-                  <th></th>
+                  <th colSpan={2}>Price list for {date === today ? "today" : (date < today ? "the past" : "the future")}: {DateTimeUtils.formatDate(date)}</th>
                 </tr>
+                {renderHidePastItems()}
                 <tr>
                     <th>Hour</th>
                     <th>Price [PLN/kWh]</th>
                 </tr>
               </thead>
                 <tbody>
-                  {pricelist.map((pricelistItem: PricelistItem) => (
+                  {filteredPricelist.map((pricelistItem: PricelistItem) => (
                       <tr key={pricelistItem.startsAt}>
                           <td>{DateTimeUtils.getTime(pricelistItem.startsAt)}</td>
                           <td>{CurrencyUtils.format(pricelistItem.price)}</td>
