41adc623c04ba2dae0b865eb449924844b7bfae1
Tomasz Wozniak <tomasz.wozniak@bp.com>
2024-10-18 13:34:42 +0200

    audi service

---
+++ server/src/audi-tracker/services/AudiService.ts
@@ -0,0 +1,56 @@
+import { UserError } from "../../services/UserError";
+import { JobService } from "./JobService";
+import { DateTimeUtils } from "smart-power-consumer-api";
+
+
+export class AudiService extends JobService{
+
+    protected static accessToken?: string; 
+    protected refreshToken: string; 
+    protected vehicleId: string; 
+    protected clientId: string; 
+    protected userAgent: string; 
+    protected url = `https://emea.bff.cariad.digital/`;
+
+    public constructor(id: string) {
+        super(id)
+        this.vehicleId = process.env.vehicleId;
+        this.clientId = process.env.clientId;
+        this.userAgent = process.env.userAgent;
+        this.refreshToken = process.env.refreshToken;
+    }
+
+        private async refreshAccessToken(): Promise<string> {
+        const path = `login/v1/idk/token`;
+        const headers = {
+                "accept": "application/json",
+                "Accept-encoding": "gzip",
+                "accept-charset": "utf-8",
+                "content-type": "application/x-www-form-urlencoded",
+                "user-agent": this.userAgent, 
+            };
+        
+        const data = new URLSearchParams();
+        data.append('client_id', this.clientId);
+        data.append('grant_type', 'refresh_token');
+        data.append('response_type', 'token id_token');
+        data.append('refresh_token', this.refreshToken);
+
+        const response = await fetch(this.url+path, { method: "post", headers, body: data }) ;
+        if (response.ok) {
+            const json = await response.json()
+            return json.access_token;
+        } else {
+            throw new UserError(`refreshAccessToken failed${response.statusText}`);
+        }
+    }
+
+    protected dateToModifiedSince = (date: number) => {
+        const gmtDate = new Date(date - 2 * 60 * 60 * 1000);
+        const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const timeComponent = DateTimeUtils.formatTimeWithSecs(gmtDate.getTime());
+        return `${dayNames[gmtDate.getDay()]}, ${gmtDate.getDate()} ${monthNames[gmtDate.getMonth()]} ${gmtDate.getFullYear()} ${timeComponent} GMT`
+    }
+
+}
--- server/src/audi-tracker/services/LocationTrackerService.ts
+++ server/src/audi-tracker/services/LocationTrackerService.ts
@@ -1,8 +1,8 @@
 import { DateTimeUtils } from "smart-power-consumer-api";
 import { UserError } from "../../services/UserError";
-import { JobService } from "./JobService";
 import fs from 'node:fs';
 import { GpxFormat } from "./GpxFormat";
+import { AudiService } from "./AudiService";
 
 
 export interface AudiLocation {
@@ -12,60 +12,17 @@ export interface AudiLocation {
   lastRefresh?: number
 }
 
-export class LocationTrackerService extends JobService {
+export class LocationTrackerService extends AudiService {
 
-    private accessToken?: string; 
-    private refreshToken: string; 
-    private vehicleId: string; 
-    private clientId: string; 
-    private userAgent: string; 
-    private url = `https://emea.bff.cariad.digital/`
     private locations: AudiLocation[] = [];
     private currentDay?: number;
     private lastRefresh: number;
 
     public constructor(interval: number = 15 * 60 * 1000) {
         super("location-tracker", interval);
-        this.vehicleId = process.env.vehicleId;
-        this.clientId = process.env.clientId;
-        this.userAgent = process.env.userAgent;
-        this.refreshToken = process.env.refreshToken;
         this.lastRefresh = Date.now() - 60*60*1000;
     }
 
-
-    private async refreshAccessToken(): Promise<string> {
-        const path = `login/v1/idk/token`;
-        const headers = {
-                "accept": "application/json",
-                "Accept-encoding": "gzip",
-                "accept-charset": "utf-8",
-                "content-type": "application/x-www-form-urlencoded",
-                "user-agent": this.userAgent, 
-            };
-        
-        const data = new URLSearchParams();
-        data.append('client_id', this.clientId);
-        data.append('grant_type', 'refresh_token');
-        data.append('response_type', 'token id_token');
-        data.append('refresh_token', this.refreshToken);
-
-        const response = await fetch(this.url+path, { method: "post", headers, body: data }) ;
-        if (response.ok) {
-            const json = await response.json()
-            return json.access_token;
-        } else {
-            throw new UserError(`refreshAccessToken failed${response.statusText}`);
-        }
-    }
-
-    private dateToModifiedSince = (date: number) => {
-        const gmtDate = new Date(date - 2 * 60 * 60 * 1000);
-        const dayNames = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-        const timeComponent = DateTimeUtils.formatTimeWithSecs(gmtDate.getTime());
-        return `${dayNames[gmtDate.getDay()]}, ${gmtDate.getDate()} ${monthNames[gmtDate.getMonth()]} ${gmtDate.getFullYear()} ${timeComponent} GMT`
-    }
 
     private async fetchParkingPosition(lastRefresh: number): Promise<AudiLocation | null> {
         const path = `vehicle/v1/vehicles/${this.vehicleId}/parkingposition`;
