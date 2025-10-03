2f5455458cedbf5804aae09b51f1a7fa2b6abe99
Tomasz Wozniak <tomasz.wozniak@bp.com>
2025-10-03 21:09:21 +0200

    XlsxRdnPricelistProvider

--- server/src/WebServer.ts
+++ server/src/WebServer.ts
@@ -4,23 +4,22 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { PricelistCtrl } from './controlers/PricelistCtrl';
 import { TariffSelectorPricelist } from './services/TariffSelectorPricelist';
-import { W12PricelistProvider } from './services/W12PricelistProvider';
 import { PowerConsumersCtrl } from './controlers/PowerConsumersCtrl';
 import { PowerConsumersService } from './services/PowerConsumersService';
 import { TimePeriodPricelistService } from './services/TimePeriodPricelistService';
-import { RdnPricelistProvider } from './services/RdnPricelistProvider';
 import 'dotenv/config'
 import fs from 'node:fs';
 import { JobControler } from './audi-tracker/controlers/JobControler';
 import { LocationTrackerService } from './audi-tracker/services/LocationTrackerService';
 import { ChargingTrackerService } from './audi-tracker/services/ChargingTrackerService';
 import { ChargingTrackerControler } from './audi-tracker/controlers/ChargingTrackerControler';
+import { XlsxRdnPricelistProvider } from './services/XlsxRdnPricelistProvider';
 
 
 const webServer = express();
 webServer.use(bodyParser.json());
 
-const singleDayPricelistService = new TariffSelectorPricelist( new RdnPricelistProvider());;
+const singleDayPricelistService = new TariffSelectorPricelist( new XlsxRdnPricelistProvider());;
 const pricelistCtrl = new PricelistCtrl(singleDayPricelistService);
 const powerConsumersCtrl = new PowerConsumersCtrl(new PowerConsumersService(new TimePeriodPricelistService(singleDayPricelistService)));
 
